6f44f1445dfae52e16693fe972ead10acfb59ba3
Sergey Batenin <inferno.87@mail.ru>
2022-08-13 09:44:43 +0300

    HomeWork2

--- src/HomeWork2/Task1.java
+++ src/HomeWork2/Task1.java
@@ -1,2 +1,40 @@
-package HomeWork2;public class Task1 {
+package HomeWork2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+ * You may return the answer in any order.
+ */
+public class Task1 {
+    //static List<Integer> temp = new ArrayList<>();
+    static List<List<Integer>> result = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        System.out.println(combine(4, 2));
+        result.clear();
+        System.out.println(combine(1, 1));
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+
+        generate(n, k, 1, new ArrayList<Integer>());
+
+        return result;
+    }
+
+    public static void generate(int n, int k, int prevElement, List<Integer> temp) {
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+        }
+
+        for (int i = prevElement; i <= n; i++) {
+            temp.add(i);
+            generate(n, k, i + 1, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
 }
--- src/HomeWork2/Task2.java
+++ src/HomeWork2/Task2.java
@@ -1,2 +1,72 @@
-package HomeWork2;public class Task2 {
+package HomeWork2;
+
+/*
+ * Merge Sort
+ */
+
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int[] array = {5, 1, 4, 2, 3};
+        array = mergeSort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    private static int[] mergeSort(int[] array) {
+
+        int length = array.length;
+
+        int[] sortedArray = merge(array, 0, length - 1);
+
+        return sortedArray;
+    }
+
+    private static int[] merge(int[] array, int start, int end) {
+
+        if (start == end) {
+            return new int[]{array[start]};
+        }
+
+        int mid = (start + end) / 2;
+
+        int[] leftPart = merge(array, start, mid);
+        int[] rightPart = merge(array, mid + 1, end);
+
+        return assembling(leftPart, rightPart);
+
+    }
+
+    private static int[] assembling(int[] leftPart, int[] rightPart) {
+
+        int index = 0;
+
+        int leftIndex = 0;
+        int rigthIndex = 0;
+
+        int leftLength = leftPart.length;
+        int rigthLength = rightPart.length;
+
+        int[] result = new int[leftLength + rigthLength];
+
+        while (leftIndex < leftLength && rigthIndex < rigthLength) {
+            if (leftPart[leftIndex] < rightPart[rigthIndex]) {
+                result[index++] = leftPart[leftIndex++];
+            } else {
+                result[index++] = rightPart[rigthIndex++];
+            }
+        }
+
+        while (leftIndex < leftLength) {
+            result[index++] = leftPart[leftIndex++];
+        }
+        while (rigthIndex < rigthLength) {
+            result[index++] = rightPart[rigthIndex++];
+        }
+
+        return result;
+
+    }
 }
